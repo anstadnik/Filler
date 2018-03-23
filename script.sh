7e46ac365869a9a0cced5d2a0ee3ed15f3db5c49
Andrii STADNIK <astadnik@student.unit.ua>
2018-03-23 15:09:34 +0200

    Removed trash

--- script.sh
+++
@@ -1,6 +0,0 @@
-./resources/filler_vm -f resources/maps/map01 -p1 resources/players/abanlin.filler      -p2 ./astadnik.filler -s 5467 -t 10 | ./vis &
-./resources/filler_vm -f resources/maps/map01 -p1 resources/players/carli.filler        -p2 ./astadnik.filler -s 5467 -t 10 | ./vis &
-./resources/filler_vm -f resources/maps/map01 -p1 resources/players/champely.filler     -p2 ./astadnik.filler -s 5467 -t 10 | ./vis &
-./resources/filler_vm -f resources/maps/map01 -p1 resources/players/grati.filler        -p2 ./astadnik.filler -s 2712 -t 10 | ./vis &
-./resources/filler_vm -f resources/maps/map01 -p1 resources/players/hcao.filler         -p2 ./astadnik.filler -s 7796 -t 10 | ./vis &
-./resources/filler_vm -f resources/maps/map01 -p2 resources/players/superjeannot.filler -p1 ./astadnik.filler -t 10 | ./vis &
